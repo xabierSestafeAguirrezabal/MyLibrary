5c7a08a269a295f0b3e59b59acac8214397d8e35
iker <iker.garay@ironchip.net>
2022-08-03 12:49:00 +0200

    Initial Commit

---
+++ File.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  MyLibrary
+//
+//  Created by Jose F Gómez Arbaizar on 3/8/22.
+//
+
+import Foundation
+import LBFraudSDKiOS
+
+public class test {
+    init(){
+        let locationManager = CLLocationManager()
+              locationManager.requestWhenInUseAuthorization()
+        
+        let iron = LBFraudSDK.init(apikey: "OyjC29.PVag8bjVdYNYciah8JDYSNvhtGZEN104LGjOPqwzvrrVU8SG00")
+        let data: [String: Any] = [
+            "concept": "example1",
+            "amount": "2",
+            "operation": "transaction"
+        ]
+        _ = iron.permissionStatus()
+        iron.transactionPost(transactionId: "9273dghsg8hj", userId: "pepe", extraData: data)
+    }
+}
